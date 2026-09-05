81d5f427f78e0b46acdee215af75395d39645f70
Roge2238 <19974806703@163.com>
2026-09-05 11:07:29 +0800

    完善加入玩家匹配队列的逻辑

--- Tinytalk-rebuild/game_server.cpp
+++ Tinytalk-rebuild/game_server.cpp
@@ -2,32 +2,47 @@
 
 
 
-void GameManager::add_player_list(Session* sn)
+void GameMananger::add_player_table(std::string user_id)
 {
     std::shared_ptr<Player> p;
     {
-        lock_guard<std::mutex> lock(player_table_mtx);
-        auto it = player_table.find(sn->user_id);
-        if (it != player_table.end())
+        lock_gaurd<std::mutex>  lock(player_table_mtx);
+        aut it = player_table.find(user_id);
+        if(it! = player_table.end())
         {
             p = it->second;   // 复用已有玩家对象
-        }
-        else
+        }else
         {
             p = std::make_shared<Player>();
-            p->ctx = sn;
-            p->id = sn->user_id;
-            player_table[sn->user_id] = p;
+            p->id = user_id;
+            player_table[user_id] = p;
         }
     }
 
+
+    /*加入匹配队列*/
+    
+    // 这个设计不知好不好 player_table里面存 Player的shared_ptr实例  
+    // 匹配队列 player_match_q 里面只存 weak_ptr指针 
+    // 设想到如果用户掉线 我们需要清理client 的Session 和 Player 所以将会进行 对PLayer实例的销毁 
+    // 此时 Player_table找到 后删除销毁  避免对局玩家 和一个僵尸在匹配 用weak_ptr 观察对象存活状态  避免对局玩家和一个僵尸对局 再用disconnected检查一波   
+    std::weak_ptr<Player> weak_p (p);
+
     {
         lock_guard<std::mutex> lock(player_match_q_mtx);
-        if (!p->queued.load() && !p->in_game.load())
+        if(!p->queued.load() && !p->in_game.load())
         {
             p->queued.store(true);
+
+            if(auto sp = weak_p.lock())
+            {
+                player_match_q.push(sp);
+                player_match_q_cv.notify_one();
+            }
+        
             player_match_q.push(p);
             player_match_q_cv.notify_one();
         }
+
     }
 }
