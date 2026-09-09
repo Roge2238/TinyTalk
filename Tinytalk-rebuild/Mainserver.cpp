d75a2a412136194df512e1a40cb88635bf00bef8
Roge2238 <19974806703@163.com>
2026-09-09 21:25:15 +0800

    加入go_running 全局变量

--- Tinytalk-rebuild/Mainserver.cpp
+++ Tinytalk-rebuild/Mainserver.cpp
@@ -1,46 +1,36 @@
 #include "server.h"
+#include "game_server.h"
 
+#include <atomic>
+#include <cstdio>
 #include <thread>
 
 #define PORT 4000
 
 
-//
-int listen_fd = -1;
+std::atomic<bool> go_running{true};
 
-
-
-
-// 服务器开启入口  
+// 服务器开启入口
 int main()
 {
     
+    setvbuf(stdout, nullptr, _IONBF, 0);
 
-int listen_fd = startup(&PORT);
+    u_short port = PORT;
+    int listen_fd = startup(&port);
+    printf("Server running on port %d\n", (int)port);
 
+    /*所有线程启动*/
 
+    
+    std::thread t1(connect_thread, listen_fd);
 
-/*所有线程启动*/
-std::thread t1(connect_thread, listen_fd);
-printf("Server running on port");
-//连接线程
+    // 游戏线程:匹配 + 对局
+    std::thread t2(game_thread);
+    t2.detach();
 
+    // TODO(视频):std::thread t3(video_thread, listen_fd);
 
-
-
-// 游戏线程
-std:: thread t2(game_thread,);
-printf("Game server has started");
-
-
-
-
-
-
-
-
+    t1.join();
+    return 0;
 }
-
-
-
-
